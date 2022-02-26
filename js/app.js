888bd83f205a9ddadc0ea00650ed5490656fe079
Nabila Ayaba <nabila.ayaba@gmail.com>
2022-02-26 16:01:00 -0500

    Create board and 'x' click event

--- js/app.js
+++ js/app.js
@@ -1,17 +1,37 @@
+////////////////////////////////
 // Global Variables Here
+let player = document.getElementById('label-player')
+// let user = prompt('What\'s your name?')
 
-
+let gameboard = []
+// let cell
 
 
 ////////////////////////////////
 // Functions For Game Logic Here
 
+// set user
+// player.innerHTML = user
 
+// make board
+for (i=0; i<9; i++) {
+    let cell = document.getElementById(`gc${i}`)
+    gameboard.push(cell)
+}
 
 
 ////////////////////////////////
 // Event Listeners Here
 
-
+// add click even to gameboard cells
+for (i=0; i<gameboard.length; i++){  
+    // cell = gameboard[i]
+    let logClick = (Event) => {
+        // console.log(`clicked ${Event.target.id}`)
+        Event.target.innerHTML = `X`
+    }
+    gameboard[i].addEventListener('click', logClick)
+}
 
 ////////////////////////////////
+console.log(gameboard)
