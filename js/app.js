fc395df16307ba0fbe53a126e9cc987aa0117956
Nell Flores <nell.djmf@gmail.com>
2022-02-26 00:34:11 -0500

    created the gameboard in html and css, wrote a few variables/functions/eventlistener ideas in the js file

--- js/app.js
+++ js/app.js
@@ -1,17 +1,55 @@
 // Global Variables Here
 
+const gameBoard = document.querySelectorAll('.box')
+let moon = document.querySelectorAll('.js-moon')
+let star = document.querySelectorAll('.js-star')
+let playerChoice = []
+let computerChoice = []
+
+comboOne = [0, 1, 2]
+comboTwo = [3, 4, 5]
+comboThree = [6, 7, 8]
+comboFour = [0, 3, 6]
+comboFive = [1, 4, 7]
+comboSix = [2, 5, 8]
+comboSeven = [0, 4, 8]
+comboEight = [2, 4, 6]
 
 
 
 ////////////////////////////////
 // Functions For Game Logic Here
-
-
-
+const decideWinner = () => {
+    if (playerChoice === comboOne || comboTwo || comboThree || comboFour || comboFive || comboSix || comboSeven || comboEight) {
+        console.log(`Player has won!`)
+    } else if (computerChoice === comboOne || comboTwo || comboThree || comboFour || comboFive || comboSix || comboSeven || comboEight) {
+        console.log(`Computer has won!`)
+    } else {
+        console.log(`Draw, try again`)
+    }
+        
+}
 
 ////////////////////////////////
 // Event Listeners Here
+for (let i = 0; i < gameBoard.length; i++) {
+    gameBoard[i].addEventListener('click', () => {
+        gameBoard.push(playerChoice[i])
+        star[i].style.opacity = 1
+        console.log('player picked')
+    })
+}
 
+for (let i = 0; i < gameBoard.length; i++) {
+    gameBoard[i].addEventListener('click', () => {
+        let = computerMove = () => {
+            Math.floor(Math.random() * 8)
+            gameBoard.push(playerChoice[i])
+            moon[i].style.opacity = 1
+            console.log('computer picked')
+        }
+    })
+}
 
 
 ////////////////////////////////
