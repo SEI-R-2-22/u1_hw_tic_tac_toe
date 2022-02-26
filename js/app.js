37154a08772e871eb5dff64a27f769559936f719
Nell Flores <nell.djmf@gmail.com>
2022-02-26 16:12:27 -0500

    added some game logic

--- js/app.js
+++ js/app.js
@@ -1,28 +1,30 @@
 // Global Variables Here
 
-const gameBoard = document.querySelectorAll('.box')
-let moon = document.querySelectorAll('.js-moon')
-let star = document.querySelectorAll('.js-star')
+const gameBoard = Array.from(document.querySelectorAll('.box'))
+let moon = document.querySelectorAll('.moon')
+let star = document.querySelectorAll('.star')
 let playerChoice = []
 let computerChoice = []
 
-comboOne = [0, 1, 2]
-comboTwo = [3, 4, 5]
-comboThree = [6, 7, 8]
-comboFour = [0, 3, 6]
-comboFive = [1, 4, 7]
-comboSix = [2, 5, 8]
-comboSeven = [0, 4, 8]
-comboEight = [2, 4, 6]
-
+const winCombo = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
 
 ////////////////////////////////
 // Functions For Game Logic Here
+
 const decideWinner = () => {
-    if (playerChoice === comboOne || comboTwo || comboThree || comboFour || comboFive || comboSix || comboSeven || comboEight) {
+    if (playerChoice  === winCombo) {
         console.log(`Player has won!`)
-    } else if (computerChoice === comboOne || comboTwo || comboThree || comboFour || comboFive || comboSix || comboSeven || comboEight) {
+    } else if (computerChoice === winCombo) {
         console.log(`Computer has won!`)
     } else {
         console.log(`Draw, try again`)
@@ -32,24 +34,26 @@ const decideWinner = () => {
 
 ////////////////////////////////
 // Event Listeners Here
-for (let i = 0; i < gameBoard.length; i++) {
-    gameBoard[i].addEventListener('click', () => {
-        gameBoard.push(playerChoice[i])
-        star[i].style.opacity = 1
-        console.log('player picked')
-    })
-}
 
 for (let i = 0; i < gameBoard.length; i++) {
     gameBoard[i].addEventListener('click', () => {
         let = computerMove = () => {
             Math.floor(Math.random() * 8)
-            gameBoard.push(playerChoice[i])
+            gameBoard[i] = computerChoice[i]
             moon[i].style.opacity = 1
             console.log('computer picked')
         }
     })
 }
 
+for (let i = 0; i < gameBoard.length; i++) {
+    gameBoard[i].addEventListener('click', () => {
+        const select = playerChoice.push(gameBoard[i])
+        console.log(playerChoice[i])
+        star[i].style.opacity = 1
+        console.log('player picked')
+    })
+    
+}
 
 ////////////////////////////////
