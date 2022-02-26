f1cb89cdfb6e5a4c9707e3629d76a1ca7f8d26c6
Mark Kennedy <kennedymark@hotmail.com>
2022-02-26 11:56:16 -0500

    Added winEval logic.

--- js/app.js
+++ js/app.js
@@ -10,9 +10,107 @@ const arena = []
 ////////////////////////////////
 // Functions For Game Logic Here
 
-const winEval = () => {
-  if (arena[0] === arena[1] && arena[0] === arena[2]) {
-    console.log('You have won!')
+const winEval = (position) => {
+  switch (position) {
+    case 0:
+      if (arena[0] === arena[1] && arena[0] === arena[2]) {
+        let winner = arena[0]
+        console.log(`${winner} has won!`)
+      } else if (arena[0] === arena[3] && arena[0] === arena[6]) {
+        let winner = arena[0]
+        console.log(`${winner} has won!`)
+      } else if (arena[0] === arena[4] && arena[0] === arena[8]) {
+        let winner = arena[0]
+        console.log(`${winner} has won!`)
+      }
+    //break?
+    case 1:
+      if (arena[1] === arena[0] && arena[1] === arena[2]) {
+        let winner = arena[1]
+        console.log(`${winner} has won!`)
+      } else if (arena[1] === arena[4] && arena[1] === arena[7]) {
+        let winner = arena[1]
+        console.log(`${winner} has won!`)
+      }
+    //break?
+    case 2:
+      if (arena[2] === arena[0] && arena[2] === arena[1]) {
+        let winner = arena[2]
+        console.log(`${winner} has won!`)
+      } else if (arena[2] === arena[5] && arena[2] === arena[8]) {
+        let winner = arena[2]
+        console.log(`${winner} has won!`)
+      } else if (arena[2] === arena[4] && arena[2] === arena[6]) {
+        let winner = arena[2]
+        console.log(`${winner} has won!`)
+      }
+    //break?
+    case 3:
+      if (arena[3] === arena[0] && arena[3] === arena[6]) {
+        let winner = arena[3]
+        console.log(`${winner} has won!`)
+      } else if (arena[3] === arena[4] && arena[3] === arena[5]) {
+        let winner = arena[3]
+        console.log(`${winner} has won!`)
+      }
+    //break?
+    case 4:
+      if (arena[4] === arena[1] && arena[4] === arena[7]) {
+        let winner = arena[4]
+        console.log(`${winner} has won!`)
+      } else if (arena[4] === arena[3] && arena[4] === arena[5]) {
+        let winner = arena[4]
+        console.log(`${winner} has won!`)
+      } else if (arena[4] === arena[0] && arena[4] === arena[8]) {
+        let winner = arena[4]
+        console.log(`${winner} has won!`)
+      } else if (arena[4] === arena[2] && arena[4] === arena[6]) {
+        let winner = arena[4]
+        console.log(`${winner} has won!`)
+      }
+    //break?
+    case 5:
+      if (arena[5] === arena[2] && arena[5] === arena[8]) {
+        let winner = arena[5]
+        console.log(`${winner} has won!`)
+      } else if (arena[5] === arena[3] && arena[5] === arena[4]) {
+        let winner = arena[5]
+        console.log(`${winner} has won!`)
+      }
+    //break?
+    case 6:
+      if (arena[6] === arena[0] && arena[6] === arena[3]) {
+        let winner = arena[6]
+        console.log(`${winner} has won!`)
+      } else if (arena[6] === arena[4] && arena[6] === arena[2]) {
+        let winner = arena[6]
+        console.log(`${winner} has won!`)
+      } else if (arena[6] === arena[7] && arena[6] === arena[8]) {
+        let winner = arena[6]
+        console.log(`${winner} has won!`)
+      }
+    //break?
+    case 7:
+      if (arena[7] === arena[1] && arena[7] === arena[4]) {
+        let winner = arena[7]
+        console.log(`${winner} has won!`)
+      } else if (arena[7] === arena[6] && arena[7] === arena[8]) {
+        let winner = arena[7]
+        console.log(`${winner} has won!`)
+      }
+    //break?
+    case 8:
+      if (arena[8] === arena[2] && arena[8] === arena[5]) {
+        let winner = arena[8]
+        console.log(`${winner} has won!`)
+      } else if (arena[8] === arena[6] && arena[8] === arena[7]) {
+        let winner = arena[8]
+        console.log(`${winner} has won!`)
+      } else if (arena[8] === arena[0] && arena[8] === arena[4]) {
+        let winner = arena[8]
+        console.log(`${winner} has won!`)
+      }
+    //break?
   }
 }
 
@@ -36,12 +134,14 @@ for (let i = 0; i < squares.length; i++) {
     squares[i].innerText = selection
     arena[position] = selection
 
-    winEval()
+    winEval(position)
+
+    //console.log(i)
 
     // console.log(arena)
     // console.log(selectionArray)
     // console.log(selection)
-    // console.log(position)
+    console.log(position)
   })
 }
 
