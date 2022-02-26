3e91fcef7361da6522ff70b8bcf19742c4a5ce08
Mark Kennedy <kennedymark@hotmail.com>
2022-02-26 09:10:55 -0500

    Added the arena array, took out the switch choice

--- js/app.js
+++ js/app.js
@@ -1,44 +1,34 @@
 // Global Variables Here
 
 let squareClick = document.querySelectorAll('.square')
-const selectionArray = ['x']
+const selectionArray = ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x']
 let previousSelection = selectionArray[0]
 
+const arena = []
+
 //let currentSelection = selectionArray.push(currentSelection)
-
-if (previousSelection === 'x') {
-  let currentSelection = 'o'
-  selectionArray.push(currentSelection)
-} else {
-  let currentSelection = 'x'
-  selectionArray.push(currentSelection)
-}
-
-// switch (previousSelection) {
-//   case 'x':
-//     let currentSelection = 'o'
-//     selectionArray.push(currentSelection)
-//   case 'o':
-//     let currentSelection = 'x'
-//     selectionArray.push(currentSelection)
-// }
-
-console.log(selectionArray)
-
-// if (selectionArray[0] === 'x') {
-
-// }
 
 ////////////////////////////////
 // Functions For Game Logic Here
 
-for (let i = 0; i < squareClick.length; i++) {
-  squareClick[i].addEventListener('click', () => {
-    squareClick[i].innerText = selectionArray[i]
-  })
-}
+// Below was used to switch x and o, not sure if I needed it.
+// if (previousSelection === 'x') {
+//   let currentSelection = 'o'
+//   selectionArray.push(currentSelection)
+// } else {
+//   let currentSelection = 'x'
+//   selectionArray.push(currentSelection)
+// }
 
 ////////////////////////////////
 // Event Listeners Here
+
+for (let i = 0; i < squareClick.length; i++) {
+  squareClick[i].addEventListener('click', () => {
+    squareClick[i].innerText = selectionArray[i]
+    arena.push(selectionArray[i])
+    console.log(arena)
+  })
+}
 
 ////////////////////////////////
