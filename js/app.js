01168790ac522eb5e939a3f72804fd2b5c3bfd42
Jenna Leopold <jleopold424@gmail.com>
2022-02-26 08:26:47 -0500

    dynamically build board in JS

--- js/app.js
+++ js/app.js
@@ -1,17 +1,20 @@
 // Global Variables Here
-
-
-
+const board = document.querySelector('.ttt-board')
 
 ////////////////////////////////
 // Functions For Game Logic Here
+const buildBoard = () => {
+  for (let i = 0; i < 9; i++) {
+    const cell = document.createElement('div')
+    cell.classList.add('ttt-cell')
+    cell.setAttribute('id', `ttt-cell-${i}`)
+    board.appendChild(cell)
+  }
+}
 
-
-
+buildBoard()
 
 ////////////////////////////////
 // Event Listeners Here
-
-
 
 ////////////////////////////////
