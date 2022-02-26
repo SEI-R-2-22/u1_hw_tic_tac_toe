cf901b878358e895c81a30bec585241cb5d8a457
Guillermo Palencia <guillermopalencia@yahoo.com>
2022-02-26 13:40:10 -0500

    made tic tac toe grid and working event listener for button 1

--- js/app.js
+++ js/app.js
@@ -1,17 +1,12 @@
 // Global Variables Here
-
-
-
+const button1 = document.querySelector('.box1')
 
 ////////////////////////////////
 // Functions For Game Logic Here
-
-
-
-
+button1.addEventListener('click', () => {
+  console.log('hey you clicked me!')
+})
 ////////////////////////////////
 // Event Listeners Here
-
-
 
 ////////////////////////////////
