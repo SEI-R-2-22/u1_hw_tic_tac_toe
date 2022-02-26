e29c9a2744b0fb9865e64a92278446b481cabc8e
Mark Kennedy <kennedymark@hotmail.com>
2022-02-25 19:20:47 -0500

    Created board, and started click events.

--- js/app.js
+++ js/app.js
@@ -1,17 +1,35 @@
 // Global Variables Here
 
+let squareClick = document.querySelectorAll('.square')
 
+const selectionArray = ['x']
+let previousSelection = selectionArray[0]
 
+//let currentSelection = selectionArray.push(currentSelection)
+
+switch (previousSelection) {
+  case 'x':
+    selectionArray.push('o')
+  default:
+    selectionArray.push('x')
+}
+
+console.log(selectionArray)
+
+// if (selectionArray[0] === 'x') {
+
+// }
 
 ////////////////////////////////
 // Functions For Game Logic Here
 
-
-
+for (let i = 0; i < squareClick.length; i++) {
+  squareClick[i].addEventListener('click', () => {
+    squareClick[i].innerText = 'X'
+  })
+}
 
 ////////////////////////////////
 // Event Listeners Here
-
-
 
 ////////////////////////////////
