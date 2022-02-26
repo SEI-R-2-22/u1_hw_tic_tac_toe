a3268e5fd8f0c9887b6a26177c474c44f2ac203d
Chinwendu <chinwenduukoha@gmail.com>
2022-02-26 04:33:43 -0500

    halfway there

--- js/app.js
+++ js/app.js
@@ -1,17 +1,113 @@
 // Global Variables Here
 
+//game tools
+let hasGameStarted = false
+let isTwoPlayer = true
 
+//Variables for Whose turn it is
+let turnIndex = 0
+let whosTurnIsIt = []
+whosTurnIsIt[0] = 'X'
+whosTurnIsIt[1] = 'O'
 
+// Variables for timer
+let timer = null
 
+//variables for scoring
+var ScoreX = 0
+var ScoreO = 0
+var ScoreD = 0
+var whoWon = ''
 ////////////////////////////////
 // Functions For Game Logic Here
+function startGame() {
+  hasGameStarted = true
+  let sg = document.getElementById('hideable')
+  sg.style.display = 'none'
+  UpdateGameMessage('' + WhoseTurn() + "'s turn")
+  startTimer()
+}
 
+function playerCount() {
+  isTwoPlayer = !isTwoPlayer
+  let pS = document.getElementById('playerSelection')
+  pS.innerHTML =
+    'Click to Choose Number of Players: ' +
+    (isTwoPlayer ? '1p v 2p' : '1p v Cpu')
+}
+function ticTacToeBoard(spaceValue) {
+  //TODO no repeats
+  if (hasGameStarted) {
+    document.getElementsByClassName('space' + spaceValue)[0].innerHTML =
+      WhoseTurn()
+    CheckForWinner()
+  }
+}
 
+function WhoseTurn() {
+  return whosTurnIsIt[turnIndex]
+}
 
+function NextPlayer() {
+  turnIndex += 1
+  turnIndex %= whosTurnIsIt.length
+  UpdateGameMessage('' + WhoseTurn() + "'s turn")
+  startTimer()
+}
 
+function UpdateGameMessage(message) {
+  let gM = document.getElementById('gameMessage')
+  gM.innerHTML = message
+}
+
+function startTimer() {
+  stopTimer()
+  let countDownTime = 10
+
+  timer = setInterval(function () {
+    console.log(countDownTime)
+
+    if (countDownTime < 0) {
+      clearInterval(timer)
+      UpdateGameMessage("Time's Up!")
+      //TODO:
+      //AutoPlay()
+    } else {
+      document.getElementById('gameTimer').innerHTML = countDownTime
+    }
+
+    countDownTime -= 1
+  }, 1000)
+}
+function stopTimer() {
+  clearInterval(timer)
+  document.getElementById('gameTimer').innerHTML = ''
+}
+
+function CheckForWinner() {
+  console.log('checking winner')
+  if (checkBoardForWinners()) {
+    //win condition
+    GameOver()
+    console.log('somebody one')
+  } else {
+    NextPlayer()
+    console.log('no winner yet')
+  }
+}
+
+function GameOver() {}
+function checkBoardForWinners() {
+  var result = false
+  var winner = 'D'
+  //TODO:
+  //game logic
+  if (result) {
+    whoWon = winner
+  }
+  return result
+}
 ////////////////////////////////
 // Event Listeners Here
-
-
 
 ////////////////////////////////
