4841d8fe0508c7852f193eec0a8cf6f3e82cfa34
Mark Harmon <neothej1sec@gmail.com>
2022-02-26 21:00:57 -0600

    eventlisteners not set up yet

--- js/app.js
+++ js/app.js
@@ -1,11 +1,31 @@
 // Global Variables Here
+let s0 = document.querySelector('#sq0')
+let s1 = document.querySelector('#sq0')
+let s2 = document.querySelector('#sq0')
+let s3 = document.querySelector('#sq0')
+let s4 = document.querySelector('#sq0')
+let s5 = document.querySelector('#sq0')
+let s6 = document.querySelector('#sq0')
+let s7 = document.querySelector('#sq0')
+let s8 = document.querySelector('#sq0')
+const board = [s0, s1, s2, s3, s4, s5, s6, s7, s8];
 
-
+// const sayHello = () => {alert('yay')}
+// s0.addEventListener('click', (sayHello) => {
+//     document.querySelector('#sq0').append('X');
+// }
 
 
 ////////////////////////////////
 // Functions For Game Logic Here
-
+function addEveL(){
+    for(let i = 0; i < 9; i++){
+        board[i].addEventListener ('click', ('wow') => {
+            alert{'duh'}
+        })
+    }
+}
+addEveL();
 
 
 
