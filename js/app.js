8196609f6834dbf6d12f1d70f0e3d53cfa37dbe9
Chinwendu <chinwenduukoha@gmail.com>
2022-02-26 23:20:42 -0500

    almost there

--- js/app.js
+++ js/app.js
@@ -18,17 +18,33 @@ let ScoreX = 0
 let ScoreO = 0
 let ScoreD = 0
 let whoWon = ''
+
+//array of winning scenarios
+let winningLines = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7]
+]
+
 ////////////////////////////////
 // Functions For Game Logic Here
 function startGame() {
+  clearBoard()
   hasGameStarted = true
-  let sg = document.getElementById('hideable')
-  sg.style.display = 'none'
+  //TODO change hide show button to function
+  let sG = document.getElementById('hideable')
+  sG.style.display = 'none'
   UpdateGameMessage('' + WhoseTurn() + "'s turn")
   startTimer()
 }
 
 function playerCount() {
+  //TODO add cpu logic
   isTwoPlayer = !isTwoPlayer
   let pS = document.getElementById('playerSelection')
   pS.innerHTML =
@@ -36,8 +52,10 @@ function playerCount() {
     (isTwoPlayer ? '1p v 2p' : '1p v Cpu')
 }
 function ticTacToeBoard(spaceValue) {
-  //TODO no repeats
-  if (hasGameStarted) {
+  //TODO add 'error' message if user picks occupied cell
+  let tempBoardVal = document.getElementsByClassName('space' + spaceValue)[0]
+    .innerHTML
+  if (hasGameStarted && tempBoardVal === '') {
     document.getElementsByClassName('space' + spaceValue)[0].innerHTML =
       WhoseTurn()
     CheckForWinner()
@@ -52,7 +70,15 @@ function NextPlayer() {
   turnIndex += 1
   turnIndex %= whosTurnIsIt.length
   UpdateGameMessage('' + WhoseTurn() + "'s turn")
-  startTimer()
+  if (isTwoPlayer) {
+    startTimer()
+  } else {
+    if (WhoseTurn() == 'O') {
+      cpuPlay()
+    } else {
+      startTimer()
+    }
+  }
 }
 
 function UpdateGameMessage(message) {
@@ -62,10 +88,11 @@ function UpdateGameMessage(message) {
 
 function startTimer() {
   stopTimer()
+
   let countDownTime = 10
 
   timer = setInterval(function () {
-    console.log(countDownTime)
+    //console.log(countDownTime)
 
     if (countDownTime < 0) {
       clearInterval(timer)
@@ -86,27 +113,134 @@ function stopTimer() {
 
 function CheckForWinner() {
   console.log('checking winner')
-  if (checkBoardForWinners()) {
+  if (checkBoardForMatches()) {
     //win condition
-    GameOver()
-    console.log('somebody one')
+    GameOver(whoWon)
+    console.log('game is done')
+  } else if (isBoardFull()) {
+    GameOver('D')
+    console.log('game is done')
   } else {
     NextPlayer()
     console.log('no winner yet')
   }
 }
 
-function GameOver() {}
-function checkBoardForWinners() {
+function GameOver(winner) {
+  stopTimer()
+  recordResults(winner)
+
+  let sG = document.getElementById('hideable')
+  sG.style.display = 'block'
+}
+
+function recordResults(winner) {
+  if (winner === 'X') {
+    ScoreX++
+    UpdateGameMessage(winner + ' is the winner')
+    console.log('somebody won')
+  } else if (winner === 'O') {
+    ScoreO++
+    UpdateGameMessage(winner + ' is the winner')
+    console.log('somebody won')
+  } else {
+    ScoreD++
+    UpdateGameMessage('Tie Game')
+    console.log('tie game')
+  }
+  document.getElementById(
+    'scoreBoard'
+  ).innerHTML = `X: ${ScoreX} \n O: ${ScoreO} \n D: ${ScoreD}`
+}
+
+function clearBoard() {
+  for (let i = 0; i < 9; i++) {
+    var key = i + 1
+    document.getElementsByClassName('space' + key)[0].innerHTML = ''
+  }
+}
+
+function checkBoardForMatches() {
   let result = false
   let winner = 'D'
-  //TODO:
-  //game logic
+
+  let boardData = getBoardData()
+
+  winningLines.forEach(function (wLine) {
+    let tempWin = isWinner(boardData, wLine)
+    if (tempWin != '') {
+      winner = tempWin
+      result = true
+    }
+  })
+
   if (result) {
     whoWon = winner
   }
   return result
 }
+
+function getBoardData() {
+  let boardDict = []
+
+  for (let i = 0; i < 9; i++) {
+    var key = i + 1
+    var boardVal = document.getElementsByClassName('space' + key)[0].innerHTML
+    //console.log('val: ' + boardVal)
+    boardDict[key] = boardVal
+  }
+
+  return boardDict
+}
+
+function isWinner(boardData, winningLine) {
+  let result =
+    boardData[winningLine[0]] === boardData[winningLine[1]] &&
+    boardData[winningLine[0]] === boardData[winningLine[2]] &&
+    boardData[winningLine[1]] === boardData[winningLine[2]] &&
+    (boardData[winningLine[0]] != '' ||
+      boardData[winningLine[1]] != '' ||
+      boardData[winningLine[2]] != '')
+
+  console.log(
+    boardData[winningLine[0]] +
+      ', ' +
+      boardData[winningLine[1]] +
+      ', ' +
+      boardData[winningLine[2]]
+  )
+  if (result) return boardData[winningLine[0]]
+  else return ''
+}
+
+function isBoardFull() {
+  let result = true
+  for (let i = 0; i < 9; i++) {
+    var key = i + 1
+    var boardVal = document.getElementsByClassName('space' + key)[0].innerHTML
+
+    if (boardVal === '') {
+      result = false
+    }
+  }
+
+  return result
+}
+
+//TODO CPU functions for single player game
+
+function AutoPlay() {
+  stopTimer()
+}
+
+function cpuPlay() {
+  stopTimer()
+}
+//function for find empty cell
+
+//function for CPU 'thinking' timer
+
+//function for time out cell selection
 ////////////////////////////////
 // Event Listeners Here
 
