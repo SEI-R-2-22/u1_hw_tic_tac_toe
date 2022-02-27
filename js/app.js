7c1f1563c5540a4a6088de22dd6da22cf02bbe77
LawrenceOL <loreganlloyd@gmail.com>
2022-02-27 11:37:32 -0500

    background color toggle added to squares

--- js/app.js
+++ js/app.js
@@ -1,9 +1,30 @@
 // Global Variables Here
 
+const gameSquares = document.querySelectorAll('.game-square')
+
 ////////////////////////////////
 // Functions For Game Logic Here
 
 ////////////////////////////////
 // Event Listeners Here
+for (let i = 0; i < gameSquares.length; i++) {
+  gameSquares[i].classList.add('empty')
+  gameSquares[i].addEventListener('click', () => {
+    if (gameSquares[i].style.backgroundColor === 'lightblue')
+      gameSquares[i].style.backgroundColor = 'yellow'
+    else gameSquares[i].style.backgroundColor = 'lightblue'
+  })
+}
 
 ////////////////////////////////
+
+const elements = document.querySelectorAll('.js-ball')
+for (let i = 0; i < elements.length; i++) {
+  elements[i].addEventListener('click', () => {
+    score = score + parseInt(elements[i].getAttribute('data-increment'))
+    document.querySelector('.js-score').innerHTML = score
+    if (score >= 100) {
+      levelWinner.style.opacity = '1'
+    }
+  })
+}
