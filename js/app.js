f3e1f9931fedf3bd2aa72c2fd52ee0ab9ec45172
rarebit3 <johntrevorbond@gmail.com>
2022-02-27 11:07:14 -0500

    first commit, html and css

--- js/app.js
+++ js/app.js
@@ -1,7 +1,22 @@
 // Global Variables Here
+const gameStatus = document.querySelector('.playerInfo')
+let crntPlayer = 'Player 1 - X'
 
 
+/*selecting each box to place an x */
+let box1 = document.querySelector('.tLft')
+let box2 = document.querySelector('.tMid')
+let box3 = document.querySelector('.tRgt')
+let box4 = document.querySelector('.mLft')
+let box5 = document.querySelector('.mid')
+let box6 = document.querySelector('.mRgt')
+let box7 = document.querySelector('.bLft')
+let box8 = document.querySelector('.bMid')
+let box9 = document.querySelector('.bRgt')
 
+const printBox = () => {
+ document.getElementById('print').innerHTML = "X"   
+}
 
 ////////////////////////////////
 // Functions For Game Logic Here
@@ -15,3 +30,6 @@
 
 
 ////////////////////////////////
+
+const inspect = document.querySelectorAll('.sqr')
+console.log(inspect)
