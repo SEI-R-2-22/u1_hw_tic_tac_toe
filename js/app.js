c8233443412cbfd31ac5433fbf84b008e502dbe8
rarebit3 <johntrevorbond@gmail.com>
2022-02-27 15:08:58 -0500

    added win logic

--- js/app.js
+++ js/app.js
@@ -1,37 +1,60 @@
 // Global Variables Here
-const gameStatus = document.querySelector('.playerInfo')
-let crntPlayer = 'Player 1 - X'
-let turnCount = 0
-let boxArray = document.getElementsByClassName('sqr');
-console.log(boxArray)
 
-/* switch to variables??
-let box1 = document.querySelector('.b1')
-let box2 = document.querySelector('.')
-let box3 = document.querySelector('.tRgt')
-let box4 = document.querySelector('.mLft')
-let box5 = document.querySelector('.mid')
-let box6 = document.querySelector('.mRgt')
-let box7 = document.querySelector('.bLft')
-let box8 = document.querySelector('.bMid')
-let box9 = document.querySelector('.bRgt')
-*/
+let turnCount = 0
+let xArray = []
+let oArray = []
+
+const winCon1 = ['b1', 'b2', 'b3']
+const winCon2 = ['b4', 'b5', 'b6']
+const winCon3 = ['b7', 'b8', 'b9']
+const winCon4 = ['b1', 'b4', 'b7']
+const winCon5 = ['b2', 'b5', 'b8']
+const winCon6 = ['b3', 'b6', 'b9']
+const winCon7 = ['b1', 'b5', 'b9']
+const winCon8 = ['b3', 'b5', 'b7']
+
+const checkWinner = (winCon) => {
+
+
+    const checkX = winCon.every(element => 
+        {return xArray.includes(element);});
+    if (checkX === true) {console.log('Player X Wins')} 
+    else {const checkO = winCon.every(element => 
+        {return oArray.includes(element);});
+    if (checkO === true) {console.log('Player O Wins')}}
+}
 
 const printBox = (box) => {
     if (turnCount % 2 === 0) {
         document.getElementById(box).innerHTML = "X";
+        xArray.push(box)
+        console.log(xArray)
     } else {
         document.getElementById(box).innerHTML = "O";
+        oArray.push(box)
+        console.log(oArray)
     }
     document.getElementById(box).disabled = true;
     turnCount++;
-}
+    checkWinner(winCon1)
+    checkWinner(winCon2)
+    checkWinner(winCon3)
+    checkWinner(winCon4)
+    checkWinner(winCon5)
+    checkWinner(winCon6)
+    checkWinner(winCon7)
+    checkWinner(winCon8)
+    }
+
+ 
+
 const resetGame = () => {
     turnCount = 0;
     document.querySelectorAll('.sqr').forEach(button => button.innerHTML = "");
     document.querySelectorAll('.sqr').forEach(button => button.disabled = false);
-  
-}
+    xArray = []
+    oArray = []
+  }
 
 
 ////////////////////////////////
