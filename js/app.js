a7a1b8f6c79d3e88ae046172e87de62776526453
Nell Flores <nell.djmf@gmail.com>
2022-02-27 04:09:41 -0500

    fixed computer logic

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 // Global Variables Here
 
-const gameBoard = Array.from(document.querySelectorAll('.box'))
+const tiles = document.querySelectorAll('.box')
+const gameBoard = Array.from(tiles)
+let moonMove = Array.from(document.getElementsByClassName('inactive'))
 let moon = document.querySelectorAll('.moon')
 let star = document.querySelectorAll('.star')
-let playerChoice = []
-let computerChoice = []
-
+let playerChoice = document.getElementsByClassName('player')
 const winCombo = [
     [0, 1, 2],
     [3, 4, 5],
@@ -32,28 +32,69 @@ const decideWinner = () => {
         
 }
 
+
 ////////////////////////////////
 // Event Listeners Here
 
-for (let i = 0; i < gameBoard.length; i++) {
-    gameBoard[i].addEventListener('click', () => {
-        let = computerMove = () => {
-            Math.floor(Math.random() * 8)
-            gameBoard[i] = computerChoice[i]
-            moon[i].style.opacity = 1
-            console.log('computer picked')
-        }
-    })
-}
+
+// const computerMove = () => {
+//         Math.floor(Math.random() * availTiles.length)
+//         computerChoice = computerMove
+//         console.log(computerChoice)
+// }
+
+
 
 for (let i = 0; i < gameBoard.length; i++) {
     gameBoard[i].addEventListener('click', () => {
-        const select = playerChoice.push(gameBoard[i])
-        console.log(playerChoice[i])
+        tiles[i].classList.remove('inactive')
+        tiles[i].classList.add('player')
         star[i].style.opacity = 1
-        console.log('player picked')
+        moon[i] = null
+        computerMove();
+        console.log(moonMove)
+    
     })
     
 }
 
+const computerMove = () => {
+    const diceRoll = moonMove[Math.floor(Math.random() * moonMove.length)]
+        diceRoll.classList.remove('inactive')
+        diceRoll.classList.add('computer')
+        console.log(diceRoll)
+        
+        diceRoll.style.opacity = 1
+}
+
+// const computerMark = () => {
+//     document.getElementsByClassName('computer').style.opacity = 1
+// }
+    
+
+
 ////////////////////////////////
+// document.addEventListener('click', let = computerMove = () => {
+//     Math.floor(Math.random() * activeTiles.length)
+//     computerChoice.push(gameBoard[i])
+//     tiles[i].classList.remove('inactive')
+//     moon[i].style.opacity = 1
+//     console.log('computer picked')
+// },
+// false
+// )
+// const computerMove = () => {
+//     Math.floor(Math.random() * activeTiles.length)
+//     computerChoice.push(gameBoard[i])
+//     tiles[i].classList.remove('inactive')
+//     moon[i].style.opacity = 1
+//     console.log('computer picked')
+// }
+
+
+// Math.floor(Math.random() * 9)
+// computerChoice.push(gameBoard[i])
+// tiles[i].classList.remove('inactive')
+// moon[i].style.opacity = 1
+// console.log(computerChoice)
+// console.log(availTiles)
