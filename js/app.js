eecb7e0ae76d793e78709f24a724a271aa3bfd9d
Mark Harmon <neothej1sec@gmail.com>
2022-02-26 22:42:07 -0600

    java confusion corrected

--- js/app.js
+++ js/app.js
@@ -1,36 +1,41 @@
 // Global Variables Here
-let s0 = document.querySelector('#sq0')
-let s1 = document.querySelector('#sq0')
-let s2 = document.querySelector('#sq0')
-let s3 = document.querySelector('#sq0')
-let s4 = document.querySelector('#sq0')
-let s5 = document.querySelector('#sq0')
-let s6 = document.querySelector('#sq0')
-let s7 = document.querySelector('#sq0')
-let s8 = document.querySelector('#sq0')
-const board = [s0, s1, s2, s3, s4, s5, s6, s7, s8];
+let sZer = document.querySelector('#sq0')
+let sOne = document.querySelector('#sq1')
+let sTwo = document.querySelector('#sq2')
+let sThr = document.querySelector('#sq3')
+let sFou = document.querySelector('#sq4')
+let sFiv = document.querySelector('#sq5')
+let sSix = document.querySelector('#sq6')
+let sSev = document.querySelector('#sq7')
+let sEig = document.querySelector('#sq8')
 
-// const sayHello = () => {alert('yay')}
-// s0.addEventListener('click', (sayHello) => {
-//     document.querySelector('#sq0').append('X');
-// }
+
+
+
 
 
 ////////////////////////////////
 // Functions For Game Logic Here
-function addEveL(){
-    for(let i = 0; i < 9; i++){
-        board[i].addEventListener ('click', ('wow') => {
-            alert{'duh'}
-        })
+playerChoice = () => {
+    let boxs = document.querySelector('.sqr')
+    switch(boxes){
+        case sZer:
+            sZer.style.color = 'blue'
     }
 }
-addEveL();
-
 
 
 ////////////////////////////////
 // Event Listeners Here
+const sayHello =() => {
+    alert("Button Clicked")
+}
+
+const btnBoard = document.getElementsByClassName('btn')
+
+for (let i = 0; i < 8; i++) {
+    btnBoard[i].addEventListener('click', sayHello)
+}
 
 
 
