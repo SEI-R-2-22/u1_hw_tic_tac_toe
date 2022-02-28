ff110f4755e5fbfd555f13eedd47eff5c337a574
Job85 <julianjernigan@ymail.com>
2022-02-28 09:05:20 -0500

    set variables

--- js/app.js
+++ js/app.js
@@ -1,16 +1,40 @@
+// A user should be able to click on different squares to make a move.
+// Every click will alternate between marking an X and O
+// A cell should not be able to be replayed once marked.
+// You should not be able to click remaining empty cells after the game is over.
+// Add a reset button that will clear the contents of the board.
+// Display a message to indicate which turn is about to be played.
+// Detect draw conditions (ties/cat's game)
+// Detect winner: Stop game and declare the winner if one player ends up getting three in a row.
+// Hint: Determine a set of winning combinations. Check those combinations on the board contents after every move.
+
+
+
 ////////////////////////////////
 // Global Variables Here
-
-
+let player
+let goFirst
+let board
+let count = 0
+let x = 0
+let o = 0
+let d = 0
+let winner
+const reset
+const x
+const o
 
 ////////////////////////////////
 // Functions For Game Logic Here
 
 
 
+
 ////////////////////////////////
 // Event Listeners Here
+//Event listener for board clicks (a selector on a section of the board)
 
+//Event listener for clicking on play again button
 
 
 ////////////////////////////////
