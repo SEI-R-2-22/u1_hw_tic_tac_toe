ab821b445a353c3115013748495a401a76fe09fb
Guillermo Palencia <guillermopalencia@yahoo.com>
2022-02-28 15:55:27 -0500

    created function to check for winning combination and announce winner

--- js/app.js
+++ js/app.js
@@ -11,6 +11,17 @@ const button9 = document.querySelector('#box9')
 const buttons = document.querySelectorAll('.box')
 const currentTurn = document.querySelector('.turn')
 
+const winningCombo = [
+  [1, 2, 3],
+  [1, 5, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [4, 5, 6],
+  [7, 8, 9],
+  [7, 5, 3]
+]
+
 ////////////////////////////////
 // Functions For Game Logic Here
 
@@ -36,7 +47,115 @@ function changeTurn(boxes) {
 function reload() {
   location.reload()
 }
-////////////////////////////////
+
+function checkCombo(boxes) {
+  if (
+    button1.innerHTML == 'X' &&
+    button2.innerHTML == 'X' &&
+    button3.innerHTML == 'X'
+  ) {
+    alert('Player 2 Wins!')
+  } else if (
+    button1.innerHTML == 'O' &&
+    button2.innerHTML == 'O' &&
+    button3.innerHTML == 'O'
+  ) {
+    alert('Player 1 Wins!')
+  }
+  if (
+    button1.innerHTML == 'X' &&
+    button5.innerHTML == 'X' &&
+    button9.innerHTML == 'X'
+  ) {
+    alert('Player 2 Wins!')
+  } else if (
+    button1.innerHTML == 'O' &&
+    button5.innerHTML == 'O' &&
+    button9.innerHTML == 'O'
+  ) {
+    alert('Player 1 Wins!')
+  }
+  if (
+    button1.innerHTML == 'X' &&
+    button4.innerHTML == 'X' &&
+    button7.innerHTML == 'X'
+  ) {
+    alert('Player 2 Wins!')
+  } else if (
+    button1.innerHTML == 'O' &&
+    button4.innerHTML == 'O' &&
+    button7.innerHTML == 'O'
+  ) {
+    alert('Player 1 Wins!')
+  }
+  if (
+    button2.innerHTML == 'X' &&
+    button5.innerHTML == 'X' &&
+    button8.innerHTML == 'X'
+  ) {
+    alert('Player 2 Wins!')
+  } else if (
+    button2.innerHTML == 'O' &&
+    button5.innerHTML == 'O' &&
+    button8.innerHTML == 'O'
+  ) {
+    alert('Player 1 Wins!')
+  }
+  if (
+    button3.innerHTML == 'X' &&
+    button6.innerHTML == 'X' &&
+    button9.innerHTML == 'X'
+  ) {
+    alert('Player 2 Wins!')
+  } else if (
+    button3.innerHTML == 'O' &&
+    button6.innerHTML == 'O' &&
+    button9.innerHTML == 'O'
+  ) {
+    alert('Player 1 Wins!')
+  }
+  if (
+    button4.innerHTML == 'X' &&
+    button5.innerHTML == 'X' &&
+    button6.innerHTML == 'X'
+  ) {
+    alert('Player 2 Wins!')
+  } else if (
+    button4.innerHTML == 'O' &&
+    button5.innerHTML == 'O' &&
+    button6.innerHTML == 'O'
+  ) {
+    alert('Player 1 Wins!')
+  }
+  if (
+    button7.innerHTML == 'X' &&
+    button8.innerHTML == 'X' &&
+    button9.innerHTML == 'X'
+  ) {
+    alert('Player 2 Wins!')
+  } else if (
+    button7.innerHTML == 'O' &&
+    button8.innerHTML == 'O' &&
+    button9.innerHTML == 'O'
+  ) {
+    alert('Player 1 Wins!')
+  }
+  if (
+    button7.innerHTML == 'X' &&
+    button5.innerHTML == 'X' &&
+    button3.innerHTML == 'X'
+  ) {
+    alert('Player 2 Wins!')
+  } else if (
+    button7.innerHTML == 'O' &&
+    button5.innerHTML == 'O' &&
+    button3.innerHTML == 'O'
+  ) {
+    alert('Player 1 Wins!')
+  }
+}
+
+//////////////
 // Event Listeners Here
 
 ////////////////////////////////
