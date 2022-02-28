3244275b3570362577a78cfca465960cdb0df5fa
canhvo16 <canhvo16@gmail.com>
2022-02-28 12:53:21 -0600

    working on scoreboard

--- js/app.js
+++ js/app.js
@@ -1,232 +1,27 @@
 // Global Variables Here
 
-////////////////////////////////
-// Functions For Game Logic Here
-
-////////////////////////////////
-// Event Listeners Here
-
-////////////////////////////////
-
 let message = document.querySelector('.turnMessage')
-
 let reset = document.querySelector('.reset')
-
-let box0 = document.querySelector('#square0')
-let box1 = document.querySelector('#square1')
-let box2 = document.querySelector('#square2')
-let box3 = document.querySelector('#square3')
-let box4 = document.querySelector('#square4')
-let box5 = document.querySelector('#square5')
-let box6 = document.querySelector('#square6')
-let box7 = document.querySelector('#square7')
-let box8 = document.querySelector('#square8')
+let squares = document.querySelectorAll('.square')
+let x = document.querySelector('#xScore')
+let o = document.querySelector('#oScore')
+let draw = document.querySelector('#drawScore')
 
 let xTurn = true
 
-let click0 = false
-let click1 = false
-let click2 = false
-let click3 = false
-let click4 = false
-let click5 = false
-let click6 = false
-let click7 = false
-let click8 = false
+let click = [false, false, false, false, false, false, false, false, false]
 
 let winner = false
 let winnerMessage = document.querySelector('.winner')
 let winnerBoard = ['', '', '', '', '', '', '', '', '']
 let squaresFilled = 0
 
-box0.addEventListener('click', () => {
-  if (winner === false) {
-    if (click0 === false) {
-      if (xTurn) {
-        box0.innerHTML = 'X'
-        winnerBoard[0] = 1
-        squaresFilled++
-        message.innerHTML = "O's Turn"
-      } else if (!xTurn) {
-        box0.innerHTML = 'O'
-        winnerBoard[0] = -1
-        squaresFilled++
-        message.innerHTML = "X's Turn"
-      }
-      xTurn = !xTurn
-      checkWin()
-      click0 = true
-    }
-  }
-})
+let xScore = 0
+let oScore = 0
+let drawScore = 0
 
-box1.addEventListener('click', () => {
-  if (winner === false) {
-    if (click1 === false) {
-      if (xTurn) {
-        box1.innerHTML = 'X'
-        winnerBoard[1] = 1
-        squaresFilled++
-        message.innerHTML = "O's Turn"
-      } else if (!xTurn) {
-        box1.innerHTML = 'O'
-        winnerBoard[1] = -1
-        squaresFilled++
-        message.innerHTML = "X's Turn"
-      }
-      xTurn = !xTurn
-      checkWin()
-      click1 = true
-    }
-  }
-})
-
-box2.addEventListener('click', () => {
-  if (winner === false) {
-    if (click2 === false) {
-      if (xTurn) {
-        box2.innerHTML = 'X'
-        winnerBoard[2] = 1
-        squaresFilled++
-        message.innerHTML = "O's Turn"
-      } else if (!xTurn) {
-        box2.innerHTML = 'O'
-        winnerBoard[2] = -1
-        squaresFilled++
-        message.innerHTML = "X's Turn"
-      }
-      xTurn = !xTurn
-      checkWin()
-      click2 = true
-    }
-  }
-})
-
-box3.addEventListener('click', () => {
-  if (winner === false) {
-    if (click3 === false) {
-      if (xTurn) {
-        box3.innerHTML = 'X'
-        winnerBoard[3] = 1
-        squaresFilled++
-        message.innerHTML = "O's Turn"
-      } else if (!xTurn) {
-        box3.innerHTML = 'O'
-        winnerBoard[3] = -1
-        squaresFilled++
-        message.innerHTML = "X's Turn"
-      }
-      xTurn = !xTurn
-      checkWin()
-      click3 = true
-    }
-  }
-})
-
-box4.addEventListener('click', () => {
-  if (winner === false) {
-    if (click4 === false) {
-      if (xTurn) {
-        box4.innerHTML = 'X'
-        winnerBoard[4] = 1
-        squaresFilled++
-        message.innerHTML = "O's Turn"
-      } else if (!xTurn) {
-        box4.innerHTML = 'O'
-        winnerBoard[4] = -1
-        squaresFilled++
-        message.innerHTML = "X's Turn"
-      }
-      xTurn = !xTurn
-      checkWin()
-      click4 = true
-    }
-  }
-})
-
-box5.addEventListener('click', () => {
-  if (winner === false) {
-    if (click5 === false) {
-      if (xTurn) {
-        box5.innerHTML = 'X'
-        winnerBoard[5] = 1
-        squaresFilled++
-        message.innerHTML = "O's Turn"
-      } else if (!xTurn) {
-        box5.innerHTML = 'O'
-        winnerBoard[5] = -1
-        squaresFilled++
-        message.innerHTML = "X's Turn"
-      }
-      xTurn = !xTurn
-      checkWin()
-      click5 = true
-    }
-  }
-})
-
-box6.addEventListener('click', () => {
-  if (winner === false) {
-    if (click6 === false) {
-      if (xTurn) {
-        box6.innerHTML = 'X'
-        winnerBoard[6] = 1
-        squaresFilled++
-        message.innerHTML = "O's Turn"
-      } else if (!xTurn) {
-        box6.innerHTML = 'O'
-        winnerBoard[6] = -1
-        squaresFilled++
-        message.innerHTML = "X's Turn"
-      }
-      xTurn = !xTurn
-      checkWin()
-      click6 = true
-    }
-  }
-})
-
-box7.addEventListener('click', () => {
-  if (winner === false) {
-    if (click7 === false) {
-      if (xTurn) {
-        box7.innerHTML = 'X'
-        winnerBoard[7] = 1
-        squaresFilled++
-        message.innerHTML = "O's Turn"
-      } else if (!xTurn) {
-        box7.innerHTML = 'O'
-        winnerBoard[7] = -1
-        squaresFilled++
-        message.innerHTML = "X's Turn"
-      }
-      xTurn = !xTurn
-      checkWin()
-      click7 = true
-    }
-  }
-})
-
-box8.addEventListener('click', () => {
-  if (winner === false) {
-    if (click8 === false) {
-      if (xTurn) {
-        box8.innerHTML = 'X'
-        winnerBoard[8] = 1
-        squaresFilled++
-        message.innerHTML = "O's Turn"
-      } else if (!xTurn) {
-        box8.innerHTML = 'O'
-        winnerBoard[8] = -1
-        squaresFilled++
-        message.innerHTML = "X's Turn"
-      }
-      xTurn = !xTurn
-      checkWin()
-      click8 = true
-    }
-  }
-})
+////////////////////////////////
+// Functions For Game Logic Here
 
 function checkWin() {
   // checks for row wins
@@ -238,6 +33,8 @@ function checkWin() {
       winner = true
       winnerMessage.innerHTML = 'Player X Wins!'
       message.style.display = 'none'
+      xScore++
+      x.innerHTML = xScore
     } else if (
       winnerBoard[rows] + winnerBoard[rows + 1] + winnerBoard[rows + 2] ===
       -3
@@ -245,6 +42,8 @@ function checkWin() {
       winner = true
       winnerMessage.innerHTML = 'Player O Wins!'
       message.style.display = 'none'
+      oScore++
+      o.innerHTML = oScore
     }
   }
   //check for column wins
@@ -258,6 +57,8 @@ function checkWin() {
       winner = true
       winnerMessage.innerHTML = 'Player X Wins!'
       message.style.display = 'none'
+      xScore++
+      x.innerHTML = xScore
     } else if (
       winnerBoard[columns] +
         winnerBoard[columns + 3] +
@@ -267,6 +68,8 @@ function checkWin() {
       winner = true
       winnerMessage.innerHTML = 'Player O Wins!'
       message.style.display = 'none'
+      oScore++
+      o.innerHTML = oScore
     }
     //check for diagonal wins
     if (
@@ -276,6 +79,8 @@ function checkWin() {
       winner = true
       winnerMessage.innerHTML = 'Player X Wins!'
       message.style.display = 'none'
+      xScore++
+      x.innerHTML = xScore
     } else if (
       winnerBoard[0] + winnerBoard[4] + winnerBoard[8] === -3 ||
       winnerBoard[2] + winnerBoard[4] + winnerBoard[6] === -3
@@ -283,14 +88,45 @@ function checkWin() {
       winner = true
       winnerMessage.innerHTML = 'Player O Wins!'
       message.style.display = 'none'
+      oScore++
+      o.innerHTML = oScore
       //check for ties
-    } else if (squaresFilled === 9) {
+    }
+    if (squaresFilled === 9) {
       message.innerHTML = ''
       winner = true
       winnerMessage.innerHTML = "It's a tie! Would you like to reset the game?"
       message.style.display = 'none'
+      drawScore++
+      draw.innerHTML = drawScore
     }
   }
+}
+
+////////////////////////////////
+// Event Listeners Here
+
+for (let i = 0; i < squares.length; i++) {
+  squares[i].addEventListener('click', () => {
+    if (winner === false) {
+      if (click[i] === false) {
+        if (xTurn) {
+          squares[i].innerHTML = 'X'
+          winnerBoard[i] = 1
+          squaresFilled++
+          message.innerHTML = "O's Turn"
+        } else if (!xTurn) {
+          squares[i].innerHTML = 'O'
+          winnerBoard[i] = -1
+          squaresFilled++
+          message.innerHTML = "X's Turn"
+        }
+        xTurn = !xTurn
+        checkWin()
+        click[i] = true
+      }
+    }
+  })
 }
 
 reset.addEventListener('click', () => {
@@ -300,15 +136,7 @@ reset.addEventListener('click', () => {
     boxes[i].innerHTML = ''
   }
 
-  click0 = false
-  click1 = false
-  click2 = false
-  click3 = false
-  click4 = false
-  click5 = false
-  click6 = false
-  click7 = false
-  click8 = false
+  click = [false, false, false, false, false, false, false, false, false]
 
   winner = false
   winnerMessage.innerHTML = ''
@@ -317,3 +145,30 @@ reset.addEventListener('click', () => {
 
   message.style.display = 'block'
 })
+
+////////////////////////////////
+
+/*
+squares.forEach((square, index) => {
+  square.addEventListener('click', () => {
+    if (winner === false) {
+      if (click0 === false) {
+        if (xTurn) {
+          box0.innerHTML = 'X'
+          winnerBoard[0] = 1
+          squaresFilled++
+          message.innerHTML = "O's Turn"
+        } else if (!xTurn) {
+          box0.innerHTML = 'O'
+          winnerBoard[0] = -1
+          squaresFilled++
+          message.innerHTML = "X's Turn"
+        }
+        xTurn = !xTurn
+        checkWin()
+        click0 = true
+      }
+    }
+  })
+})
+*/
