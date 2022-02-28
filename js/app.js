e5ea17cec7dfb183f63b7a07f63fc4e069102326
LawrenceOL <loreganlloyd@gmail.com>
2022-02-28 10:44:12 -0500

    simplified win and tie checking

--- js/app.js
+++ js/app.js
@@ -6,16 +6,15 @@ const activePlayerDisplay = document.querySelector('.active-player')
 const resetButton = document.querySelector('.reset')
 let winnerDeclared = false
 let winnerBanner = document.querySelector('.game-winner-banner h1')
+let playCount = 0
 
 ////////////////////////////////
 // Functions For Game Logic Here
 let resetGame = () => {
   for (let i = 0; i < gameSquares.length; i++) {
     gameSquares[i].innerHTML = ''
-    if (gameSquares[i].classList[1] !== 'empty') {
-      gameSquares[i].classList.add('empty')
-    }
   }
+  playCount = 0
   winnerBanner.style.opacity = 0
   activePlayer = 'x'
   activePlayerDisplay.innerHTML = 'The active player is X'
@@ -27,11 +26,18 @@ let declareWinner = () => {
   activePlayerDisplay.style.opacity = 0
   winnerBanner.innerHTML = `${activePlayer} wins!`
   winnerBanner.style.opacity = 1
-  console.log('Joe wins')
+}
+
+let declareTie = () => {
+  winnerDeclared = true
+  activePlayerDisplay.style.opacity = 0
+  winnerBanner.innerHTML = `Tie game!`
+  winnerBanner.style.opacity = 1
 }
 
 let checkWinner = () => {
   if (
+    //x win conditions
     (gameSquares[0].innerHTML === 'x' &&
       gameSquares[1].innerHTML === 'x' &&
       gameSquares[2].innerHTML === 'x') ||
@@ -55,10 +61,8 @@ let checkWinner = () => {
       gameSquares[8].innerHTML === 'x') ||
     (gameSquares[6].innerHTML === 'x' &&
       gameSquares[4].innerHTML === 'x' &&
-      gameSquares[2].innerHTML === 'x')
-  ) {
-    declareWinner()
-  } else if (
+      gameSquares[2].innerHTML === 'x') ||
+    //o win conditions
     (gameSquares[0].innerHTML === 'o' &&
       gameSquares[1].innerHTML === 'o' &&
       gameSquares[2].innerHTML === 'o') ||
@@ -85,41 +89,27 @@ let checkWinner = () => {
       gameSquares[2].innerHTML === 'o')
   ) {
     declareWinner()
-  } else if (
-    gameSquares[0].classList[1] !== 'empty' &&
-    gameSquares[1].classList[1] !== 'empty' &&
-    gameSquares[2].classList[1] !== 'empty' &&
-    gameSquares[3].classList[1] !== 'empty' &&
-    gameSquares[4].classList[1] !== 'empty' &&
-    gameSquares[5].classList[1] !== 'empty' &&
-    gameSquares[6].classList[1] !== 'empty' &&
-    gameSquares[7].classList[1] !== 'empty' &&
-    gameSquares[8].classList[1] !== 'empty' &&
-    gameSquares[8].classList[1] !== 'empty'
-  ) {
-    alert('tie game!')
   } else {
-    return
+    playCount += 1
+    console.log(gameSquares.length)
+    if (playCount === gameSquares.length) {
+      declareTie()
+    }
   }
 }
 
 ////////////////////////////////
 // Event Listeners Here
 for (let i = 0; i < gameSquares.length; i++) {
-  gameSquares[i].classList.add('empty')
+  //gameSquaresCount += 1
   gameSquares[i].addEventListener('click', () => {
     if (winnerDeclared === false) {
-      if (activePlayer === 'x' && gameSquares[i].classList[1] === 'empty') {
-        gameSquares[i].classList.remove('empty')
+      if (activePlayer === 'x' && gameSquares[i].innerHTML === '') {
         gameSquares[i].innerHTML = 'x'
         checkWinner()
         activePlayerDisplay.innerHTML = 'The active player is O'
         activePlayer = 'o'
-      } else if (
-        activePlayer === 'o' &&
-        gameSquares[i].classList[1] === 'empty'
-      ) {
-        gameSquares[i].classList.remove('empty')
+      } else if (activePlayer === 'o' && gameSquares[i].innerHTML === '') {
         gameSquares[i].innerHTML = 'o'
         checkWinner()
         activePlayerDisplay.innerHTML = 'The active player is X'
@@ -129,17 +119,4 @@ for (let i = 0; i < gameSquares.length; i++) {
   })
 }
 resetButton.addEventListener('click', resetGame)
-
 ////////////////////////////////
-
-//ball example code
-// const elements = document.querySelectorAll('.js-ball')
-// for (let i = 0; i < elements.length; i++) {
-//   elements[i].addEventListener('click', () => {
-//     score = score + parseInt(elements[i].getAttribute('data-increment'))
-//     document.querySelector('.js-score').innerHTML = score
-//     if (score >= 100) {
-//       levelWinner.style.opacity = '1'
-//     }
-//   })
-// }
