a7f667188ebf9d48f6c3d941b0d60de223f84521
Mark Kennedy <kennedymark@hotmail.com>
2022-02-28 16:45:54 -0500

    Added a score baord.

--- js/app.js
+++ js/app.js
@@ -16,10 +16,44 @@ const ticTacToe = () => {
   const arena = []
   let winner = undefined
 
-  //let currentSelection = selectionArray.push(currentSelection)
+  let scoreX = []
+  let scoreO = []
 
   ////////////////////////////////
   // Functions For Game Logic Here
+
+  let scorePush = (winner) => {
+    if (winner === '<img src="./resources/X.png" alt="X">') {
+      scoreX.push('<img src="./resources/X.png" alt="X">')
+      scoreTally(winner)
+    } else if (winner === '<img src="./resources/O.png" alt="O">') {
+      scoreO.push('<img src="./resources/O.png" alt="O">')
+      scoreTally(winner)
+    }
+  }
+
+  let scoreTally = (winner) => {
+    if (winner === '<img src="./resources/X.png" alt="X">') {
+      for (let i = 0; i < scoreX.length; i++) {
+        let scoreList = document.getElementById('scoreX')
+        let newScore = document.createElement('li')
+
+        newScore.setAttribute('class', 'tallied')
+        newScore.innerHTML = '<img src="./resources/X.png" alt="X">'
+        scoreList.appendChild(newScore)
+      }
+    } else if (winner === '<img src="./resources/O.png" alt="O">') {
+      for (let i = 0; i < scoreO.length; i++) {
+        let scoreList = document.getElementById('scoreO')
+        let newScore = document.createElement('li')
+
+        newScore.setAttribute('class', 'tallied')
+        newScore.innerHTML = '<img src="./resources/O.png" alt="O">'
+        scoreList.appendChild(newScore)
+      }
+    }
+  }
+
   let turnAnnouncement = () => {
     let announcement = document.querySelector('.turnAnnouncement')
     announcement.innerText = `Its player's ${
@@ -47,99 +81,123 @@ const ticTacToe = () => {
         if (arena[0] === arena[1] && arena[0] === arena[2]) {
           winner = arena[0]
           winnerAnnouncement(winner)
+          scorePush(winner)
         } else if (arena[0] === arena[3] && arena[0] === arena[6]) {
           winner = arena[0]
           winnerAnnouncement(winner)
+          scorePush(winner)
         } else if (arena[0] === arena[4] && arena[0] === arena[8]) {
           winner = arena[0]
           winnerAnnouncement(winner)
+          scorePush(winner)
         }
         break
       case 1:
         if (arena[1] === arena[0] && arena[1] === arena[2]) {
           winner = arena[1]
           winnerAnnouncement(winner)
+          scorePush(winner)
         } else if (arena[1] === arena[4] && arena[1] === arena[7]) {
           winner = arena[1]
           winnerAnnouncement(winner)
+          scorePush(winner)
         }
         break
       case 2:
         if (arena[2] === arena[0] && arena[2] === arena[1]) {
           winner = arena[2]
           winnerAnnouncement(winner)
+          scorePush(winner)
         } else if (arena[2] === arena[5] && arena[2] === arena[8]) {
           winner = arena[2]
           winnerAnnouncement(winner)
+          scorePush(winner)
         } else if (arena[2] === arena[4] && arena[2] === arena[6]) {
           winner = arena[2]
           winnerAnnouncement(winner)
+          scorePush(winner)
         }
         break
       case 3:
         if (arena[3] === arena[0] && arena[3] === arena[6]) {
           winner = arena[3]
           winnerAnnouncement(winner)
+          scorePush(winner)
         } else if (arena[3] === arena[4] && arena[3] === arena[5]) {
           winner = arena[3]
           winnerAnnouncement(winner)
+          scorePush(winner)
         }
         break
       case 4:
         if (arena[4] === arena[1] && arena[4] === arena[7]) {
           winner = arena[4]
           winnerAnnouncement(winner)
+          scorePush(winner)
         } else if (arena[4] === arena[3] && arena[4] === arena[5]) {
           winner = arena[4]
           winnerAnnouncement(winner)
+          scorePush(winner)
         } else if (arena[4] === arena[0] && arena[4] === arena[8]) {
           winner = arena[4]
           winnerAnnouncement(winner)
+          scorePush(winner)
         } else if (arena[4] === arena[2] && arena[4] === arena[6]) {
           winner = arena[4]
           winnerAnnouncement(winner)
+          scorePush(winner)
         }
         break
       case 5:
         if (arena[5] === arena[2] && arena[5] === arena[8]) {
           winner = arena[5]
           winnerAnnouncement(winner)
+          scorePush(winner)
         } else if (arena[5] === arena[3] && arena[5] === arena[4]) {
           winner = arena[5]
           winnerAnnouncement(winner)
+          scorePush(winner)
         }
         break
       case 6:
         if (arena[6] === arena[0] && arena[6] === arena[3]) {
           winner = arena[6]
           winnerAnnouncement(winner)
+          scorePush(winner)
         } else if (arena[6] === arena[4] && arena[6] === arena[2]) {
           winner = arena[6]
           winnerAnnouncement(winner)
+          scorePush(winner)
         } else if (arena[6] === arena[7] && arena[6] === arena[8]) {
           winner = arena[6]
           winnerAnnouncement(winner)
+          scorePush(winner)
         }
         break
       case 7:
         if (arena[7] === arena[1] && arena[7] === arena[4]) {
           winner = arena[7]
           winnerAnnouncement(winner)
+          scorePush(winner)
         } else if (arena[7] === arena[6] && arena[7] === arena[8]) {
           winner = arena[7]
           winnerAnnouncement(winner)
+          scorePush(winner)
         }
         break
       case 8:
         if (arena[8] === arena[2] && arena[8] === arena[5]) {
           winner = arena[8]
           winnerAnnouncement(winner)
+          scorePush(winner)
         } else if (arena[8] === arena[6] && arena[8] === arena[7]) {
           winner = arena[8]
           winnerAnnouncement(winner)
+          scorePush(winner)
         } else if (arena[8] === arena[0] && arena[8] === arena[4]) {
           winner = arena[8]
           winnerAnnouncement(winner)
+          scorePush(winner)
         }
         break
     }
