9985938a79c02d5d40ca63383cceed0a4faa0eed
Job85 <julianjernigan@ymail.com>
2022-02-28 16:48:07 -0500

    added logic and event listeners

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@
 // Global Variables Here
 let player
 let goFirst = -1
-let board
+let octothorp
 let count = 0
 let xWin = 0
 let oWin = 0
@@ -24,16 +24,117 @@ const playAnother = document.getElementById('playAnother')
 const X = document.getElementById('xScore')
 const O = document.getElementById('oScore')
 const D = document.getElementById('dScore')
+const message = document.getElementById('message')
 ////////////////////////////////
 // Functions For Game Logic Here
+init()
 
+function init() {
+    board = ['', '', '', '', '', '', '', '', '',]
+    goFirst *= -1
+    player = goFirst
+    X.innerText = xWin
+    O.innerText = oWin
+    D.innerText = dWin
 
+    if (goFirst === 1) {
+        message.textContent = 'x goes first'
+    } else {
+        message.textContent = 'o goes first'
+    }
+    document
+        .querySelectorAll('section.scoreBoard')[0]
+        .childNodes.forEach((element) => {
+            element.textContent = ''
+            element.color = ''
+        })
+    count = 0
+    winner = false
+}
+function gameOver() {
+    clear()
+    return (count === 0 && winner === false) || winner === true
+}
 
+function checkWinner() {
+    if (winner === false) {
+        if (
+            octothorp[0] + octothorp[1] + octothorp[2] === 3 ||
+            octothorp[0] + octothorp[3] + octothorp[6] === 3 ||
+            octothorp[0] + octothorp[4] + octothorp[8] === 3 ||
+            octothorp[1] + octothorp[4] + octothorp[7] === 3 ||
+            octothorp[2] + octothorp[5] + octothorp[8] === 3 ||
+            octothorp[3] + octothorp[4] + octothorp[5] === 3 ||
+            octothorp[0] + octothorp[1] + octothorp[2] === 3 ||
+            octothorp[6] + octothorp[7] + octothorp[8] === 3
+        ) {
+            message.textContent = 'X WINS!'
+            message.style.color = '#FFD700'
+            winner = true
+            xWin++
+            X.innerText = xWin
+        }
+        if (
+            octothorp[0] + octothorp[1] + octothorp[2] === -3 ||
+            octothorp[0] + octothorp[3] + octothorp[6] === -3 ||
+            octothorp[0] + octothorp[4] + octothorp[8] === -3 ||
+            octothorp[1] + octothorp[4] + octothorp[7] === -3 ||
+            octothorp[2] + octothorp[5] + octothorp[8] === -3 ||
+            octothorp[3] + octothorp[4] + octothorp[5] === -3 ||
+            octothorp[0] + octothorp[1] + octothorp[2] === -3 ||
+            octothorp[6] + octothorp[7] + octothorp[8] === -3
+        ) {
+            message.textContent = 'O WINS!'
+            message.style.color = '#FFD700'
+            winner = true
+            oWin++
+            O.innerText = oWin
+        }
+    }
+}
 
+function render(cell) {
+    if (winner === false) {
+        markSpot = document.getElementById(`cll${cell}`)
+        octothorp[cell] = player
+        if (player === 1) {
+            markSpot.textContent = 'X'
+            markSpot.style.color = 'FF0000'
+            message.textContent = 'O\'s Turn!'
+        } else {
+            markSpot.textContent = 'O'
+            markSpot.style.color = 'FF0000'
+            message.textContent = 'X\'s Turn!'
+        }
+    }
+}
 ////////////////////////////////
 // Event Listeners Here
 //Event listener for board clicks (a selector on a section of the board)
-
+function render(cell) {
+    if (winner === false) {
+        markSpot = document.getElementById(`cll${cell}`)
+        octothorp[cell] = player
+        if (player === 1) {
+            markSpot.textContent = 'X'
+            markSpot.style.color = 'FF0000'
+            message.textContent = 'O\'s Turn!'
+        } else {
+            markSpot.textContent = 'O'
+            markSpot.style.color = '800080'
+            message.textContent = 'X\'s Turn!'
+        }
+    }
+    player *= -1
+    checkWinner()
+    count++
+    if (count === 9 && winner === false) {
+        D.innerText = dWin
+        clear()
+        message.textContent = 'DRAW!'
+        message.style.color = 'DB7093'
+    }
+}
 //Event listener for clicking on play again button
 
 
