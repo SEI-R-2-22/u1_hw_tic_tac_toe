683e18cffa7e4aaffcf99ff277a80e0a45278696
Bryan <bryannsanchez1234@gmail.com.com>
2022-03-01 00:55:04 -0800

    completed hw

--- js/app.js
+++ js/app.js
@@ -1,17 +1,39 @@
 // Global Variables Here
+let xoro = 'x'
+const allGrid = document.querySelectorAll('.grid')
+const squares = document.querySelectorAll('.box')
+playerOne = 'x'
+playerTwo = 'o'
+/// prints x or o to the screeen when box
+const squareClick = (index) => {
+  squares[index].innerHTML = xoro
+  if (xoro === "x") {
+    xoro = "o" 
+  } else if ( xoro === 'o') {
+    xoro = "x"
+  }}
 
 
+/// gives click property to items in grid
+squares.forEach((square, idx)=> {
+  square.addEventListener('click', ()=> {
+    squareClick(idx)
+  })
+})
 
+// displays whos turn it is on the screen
+let whosTurn = (xoro + "'s turn")
+const addText = () => {
 
-////////////////////////////////
-// Functions For Game Logic Here
-
-
-
-
-////////////////////////////////
-// Event Listeners Here
-
-
-
-////////////////////////////////
+  if (xoro === "x") {
+    document.firstElementChild.append("X's Turn")
+  } else if ( xoro === 'o') {
+    document.firstElementChild.append("O's Turn")
+  }
+  document.firstElementChild.append(whosTurn)
+}
+squares.forEach((square, idx)=> {
+  square.addEventListener('click', addText)
+    
+  })
+///
---
+++ sketchpad.md
@@ -0,0 +1,6 @@
+## Tic tac toe
+
+# things i need
+
+- create some type of 3x3 grid, with clickables in each box
+- create the possible winning outcomes, in order to create a "win" or "loss"
