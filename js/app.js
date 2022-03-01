048fc1d2dc6595cf37048ef2445e11c459a679b0
ryan.ditzel <ryand370@gmail.com>
2022-02-28 21:59:15 -0500

    html js done css not done

--- js/app.js
+++ js/app.js
@@ -1,17 +1,88 @@
-// Global Variables Here
+const boxes = document.querySelectorAll('.box')
+const resetButton = document.querySelector('#reset')
+const announcer = document.querySelector('.announcer')
+let turns = 0
+let board = ['', '', '', '', '', '', '', '', ''];
+const playerX = 'X';
+const playerO = 'O';
+const currentPlayer = 'X'
+let xArray = []
+let oArray = []
+let isGameActive = true;
+const PLAYERX_WON = 'PLAYERX_WON'
+const PLAYER0_WON = 'PLAYER0_WON'
+const TIE = 'TIE'
+let roundWon = false
 
+//adds X or O to gameboard and rotates players
+for (let i = 0; i < boxes.length; i++) {
+    if (isGameActive = true) {
+    boxes[i].addEventListener('click', function(){
+        if (isGameActive) {
+            if(turns % 2 == 0){
+                let index = boxes[i].getAttribute('id')[1]
+                board[index] = 'x'
+                boxes[i].innerText = playerX
+                turns++
+                xArray.push(boxes[i])
+                checkWinner()
+            }
+            else if(turns % 2 == 1){
+                let index = boxes[i].getAttribute('id')[1]
+                board[index] = 'o'
+                boxes[i].innerText = playerO
+                turns++
+                oArray.push(boxes[i])
+                checkWinner()
+            }
+        }   
+    })
+}
 
+}
+function checkWinner () {
+   if (
+    (board[0] == 'x' && board[1] == 'x' && board[2] == 'x') ||
+    (board[3] == 'x' && board[4] == 'x' && board[5] == 'x') ||
+    (board[6] == 'x' && board[7] == 'x' && board[8] == 'x') ||
+    (board[0] == 'x' && board[3] == 'x' && board[6] == 'x') ||
+    (board[1] == 'x' && board[4] == 'x' && board[7] == 'x') ||
+    (board[2] == 'x' && board[5] == 'x' && board[8] == 'x') ||
+    (board[0] == 'x' && board[4] == 'x' && board[8] == 'x') ||
+    (board[2] == 'x' && board[4] == 'x' && board[6] == 'x')){
+        alert(PLAYERX_WON)
+        isGameActive = false
+    } else if (
+    (board[0] == 'o' && board[1] == 'o' && board[2] == 'o') ||
+    (board[3] == 'o' && board[4] == 'o' && board[5] == 'o') ||
+    (board[6] == 'o' && board[7] == 'o' && board[8] == 'o') ||
+    (board[0] == 'o' && board[3] == 'o' && board[6] == 'o') ||
+    (board[1] == 'o' && board[4] == 'o' && board[7] == 'o') ||
+    (board[2] == 'o' && board[5] == 'o' && board[8] == 'o') ||
+    (board[0] == 'o' && board[4] == 'o' && board[8] == 'o') ||
+    (board[2] == 'o' && board[4] == 'o' && board[6] == 'o')){
+        alert(PLAYERO_WON)
+        isGameActive = false
+    } else if (turns === 9) {
+        alert(TIE)
+        isGameActive = false
+    }
+}  
 
+const resetBoard = () => {
+    board = ['', '', '', '', '', '', '', '', ''];
+    isGameActive = true;
+    announcer.classList.add('hide');
 
-////////////////////////////////
-// Functions For Game Logic Here
+    if (currentPlayer === 'O') {
+        changePlayer();
+    }
 
+    boxes.forEach(tile => {
+        tile.innerText = '';
+        tile.classList.remove('playerX');
+        tile.classList.remove('playerO');
+    });
+}
 
-
-
-////////////////////////////////
-// Event Listeners Here
-
-
-
-////////////////////////////////
+resetButton.addEventListener('click', resetBoard);
