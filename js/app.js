0737fc76a833b5cf2bd9c19e2997e15bf6ff8f59
Ifeoluwa Adebisi <adebisiife@gmail.com>
2022-03-01 09:31:20 -0500

    Unstatisfied

--- js/app.js
+++ js/app.js
@@ -1,11 +1,27 @@
 ////////////////////////////////
 // Global Variables Here
+let box1 = document.querySelector('.box1')
+let box2 = document.querySelector('.box2')
+let box3 = document.querySelector('.box3')
+let box4 = document.querySelector('.box4')
+let box5 = document.querySelector('.box5')
+let box6 = document.querySelector('.box6')
+let box7 = document.querySelector('.box7')
+let box8 = document.querySelector('.box8')
+let box9 = document.querySelector('.box9')
 
+const resetB = document.querySelector('.reset-button')
 
 
 ////////////////////////////////
 // Functions For Game Logic Here
+// resetB.addEventListener('click', reload)
+function reset() {
+    let roun
+}
 
+box1.addEventListener('click', () => {
+    console.log('clicked')}, {once:true})
 
 
 ////////////////////////////////
@@ -13,4 +29,6 @@
 
 
 
+
 ////////////////////////////////
+
