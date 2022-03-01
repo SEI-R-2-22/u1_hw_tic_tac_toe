5d5ecbb4f031765316d7586baededf0b9b8aba64
Ran Kim <rannio@gmail.com>
2022-02-28 21:07:17 -0500

    completed game

--- js/app.js
+++ js/app.js
@@ -11,59 +11,143 @@ let mr = document.querySelector('.mr')
 let bl = document.querySelector('.bl')
 let bm = document.querySelector('.bm')
 let br = document.querySelector('.br')
-let winCond1 = [tl, tm, tr]
-let winCond2 = [ml, mm, mr]
-let winCond3 = [bl, bm, br]
-let winCond4 = [tl, ml, bl]
-let winCond5 = [tm, mm, bm]
-let winCond6 = [tr, mr, br]
-let winCond7 = [tl, mm, br]
-let winCond8 = [bl, mm, tr]
-let winCondition = [winCond1, winCond2, winCond3, winCond4, winCond5, winCond6, winCond7, winCond8]
+let counter = 0
+let winnerX = true
+let winnerO = true
+
 
 
 
 ////////////////////////////////
 // Functions For Game Logic Here
 
-let win = () => {
-    if (winCond1.length.ATTRIBUTE_NODE === 2) {
-        console.log('merp')
-    } else if (tl === tm && tr) {
-        console.log('O wins')
-    }
- }
 
+let replayX = () => {
+    let restartX = prompt('X won! Do you want to play again? Y/N')
+    if (restartX === 'Y') {
+        location.reload()
+    } else if (restartX ==='N') {
+        alert('Good game!')
+        location.reload()
+    } else {
+        alert ('That is not a valid answer!')
+        location.reload()
+    }
+}
+
+let replayO = () => {
+    let restartO = prompt('O won! Do you want to play again? Y/N')
+    if (restartO === 'Y') {
+        location.reload()
+    } else if (restartO ==='N') {
+        alert('Good game!')
+        location.reload()
+    } else {
+        alert ('That is not a valid answer!')
+        location.reload()
+    }
+}
+
+let replayTie = () => {
+    let restartTie = prompt('It\'s a tie! Do you want to play again? Y/N')
+    if (restartTie === 'Y') {
+        location.reload()
+    } else if (restartTie ==='N') {
+        alert('Good game!')
+        location.reload()
+    } else {
+        alert ('That is not a valid answer!')
+        location.reload()
+    }
+}
+
+let winCondX = () => {
+    replayX()
+    winnerX = false
+}
+
+let winCondO = () => {
+    replayO()
+    winnerO = false
+}
+
+let winX = () => {
+    if (tl.innerHTML === 'X' && tm.innerHTML === 'X' && tr.innerHTML === 'X') {
+        winCondX()
+    } else if (ml.innerHTML === 'X' && mm.innerHTML === 'X' && mr.innerHTML === 'X') {
+        winCondX()
+    } else if (bl.innerHTML === 'X' && bm.innerHTML === 'X' && br.innerHTML === 'X') {
+        winCondX()
+    } else if (tl.innerHTML === 'X' && ml.innerHTML === 'X' && bl.innerHTML === 'X') {
+        winCondX()
+    } else if (tm.innerHTML === 'X' && mm.innerHTML === 'X' && bm.innerHTML === 'X') {
+        winCondX()
+    } else if (tr.innerHTML === 'X' && mr.innerHTML === 'X' && br.innerHTML === 'X') {
+        winCondX()
+    } else if (tl.innerHTML === 'X' && mm.innerHTML === 'X' && br.innerHTML === 'X') {
+        winCondX()
+    } else if (bl.innerHTML === 'X' && mm.innerHTML === 'X' && tr.innerHTML === 'X') {
+        winCondX()
+    }
+}
+ 
+let winO = () => {
+    if (tl.innerHTML === 'O' && tm.innerHTML === 'O' && tr.innerHTML === 'O') {
+    winCondO()
+    } else if (ml.innerHTML === 'O' && mm.innerHTML === 'O' && mr.innerHTML === 'O') {
+        winCondO()
+    } else if (bl.innerHTML === 'O' && bm.innerHTML === 'O' && br.innerHTML === 'O') {
+        winCondO()
+    } else if (tl.innerHTML === 'O' && ml.innerHTML === 'O' && bl.innerHTML === 'O') {
+        winCondO()
+    } else if (tm.innerHTML === 'O' && mm.innerHTML === 'O' && bm.innerHTML === 'O') {
+        winCondO()
+    } else if (tr.innerHTML === 'O' && mr.innerHTML === 'O' && br.innerHTML === 'O') {
+        winCondO()
+    } else if (tl.innerHTML === 'O' && mm.innerHTML === 'O' && br.innerHTML === 'O') {
+        winCondO()
+    } else if (bl.innerHTML === 'O' && mm.innerHTML === 'O' && tr.innerHTML === 'O') {
+        winCondO()
+    }
+}
+
+let draw = () => {
+    if (counter === 9 && winnerX === true && winnerO === true) {
+        replayTie()
+    }
+}
 
 ////////////////////////////////
 // Event Listeners Here
 
-message.innerText = 'O goes first!'
 
-const start = () => {
+
+let start = () => {
+    message.innerText = 'O goes first!'
+
     for (let i = 0; i < play.length; i++) {
+        play[i].innerHTML = ''
+        player = 1
         play[i].addEventListener('click', () => {
+            counter += 1
             player *= -1
             if ( player === 1) {
                 play[i].innerHTML = 'X'
                 message.innerHTML = "O's turn"
-                win()
+                winX()
+                draw()
             } else if ( player === -1) {
                 play[i].innerHTML = 'O'
-                play[i] = 'O'
                 message.innerHTML = "X's turn"
-                win()
-            }
+                winO()
+                draw()
+            } 
         }, {once : true})
-
-
     }
-
 }
 
-
-
 start();
+
 
 
 ////////////////////////////////
