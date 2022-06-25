bf4d9f4f64dca8e8b5ec0582e20c89fd9fa8993d
Skim1571 <skim1571@gmail.com>
2022-06-25 01:37:08 -0500

    added reset and choice color

--- js/app.js
+++ js/app.js
@@ -3,11 +3,13 @@
 const sq = document.querySelectorAll('.sq');
 let isPlayerTurn = true;
 //add random turn at start of game
-//Score
+//Scoreboard variables
 let playerScore = 0;
 let cpuScore = 0;
 let tieScore = 0;
 const boardTracker = [{spot:0, mark:''},{spot:1, mark:''},{spot:2, mark:''},{spot:3, mark:''},{spot:4,mark:''},{spot:5,mark:''},{spot:6,mark:''},{spot:7,mark:''},{spot:8,mark:''}];
+//reset button
+const btn = document.getElementById('btn');
 
 ////////////////////////////////
 // Functions For Game Logic Here
@@ -35,19 +37,86 @@ if(
   || (boardTracker[2].mark === 'X' && boardTracker[4].mark === 'X' && boardTracker[6].mark === 'X') 
   ){
   alert(`Player Wins`)
+  playerScore++
+  console.log(playerScore)
+  document.getElementById('pscore').innerText = playerScore
+  restart()
   } else if ((boardTracker[0].mark === 'O' && boardTracker[1].mark === 'O' && boardTracker[2].mark === 'O') || (boardTracker[3].mark === 'O' && boardTracker[4].mark === 'O' && boardTracker[5].mark === 'O') 
     || (boardTracker[6].mark === 'O' && boardTracker[7].mark === 'O' && boardTracker[8].mark === 'O') ||
     (boardTracker[0].mark === 'O' && boardTracker[3].mark === 'O' && boardTracker[6].mark === 'O') || (boardTracker[1].mark === 'O' && boardTracker[4].mark === 'O' && boardTracker[7].mark === 'O') 
     || (boardTracker[2].mark === 'O' && boardTracker[5].mark === 'O' && boardTracker[8].mark === 'O') || (boardTracker[0].mark === 'O' && boardTracker[4].mark === 'O' && boardTracker[7].mark === 'O') 
     || (boardTracker[2].mark === 'O' && boardTracker[4].mark === 'O' && boardTracker[6].mark === 'O') ) {
   alert(`Computer Wins`)
+  cpuScore++
+  document.getElementById('cscore').innerHTML = cpuScore
+  restart()
   } else if ((boardTracker[0].mark !== '' && boardTracker[1].mark !== '' && boardTracker[2].mark !== '' && boardTracker[3].mark !== '' && boardTracker[4].mark !== '' && boardTracker[5].mark !== '') 
     && (boardTracker[6].mark !== '' && boardTracker[7].mark !== '' && boardTracker[8].mark !== '') ) {
-  alert(`The Game is a Tie`)}
+  alert(`The Game is a Tie`) 
+  tieScore++
+  document.getElementById('tscore').innerText = tieScore
+  restart()
+  }
   else{
+    turnSign()
+
   }
 }
+//displays player/cpu turn
+const turnSign = () => {
+  if(isPlayerTurn){
+    document.getElementById('pop2').style.opacity = 0
+    document.getElementById('pop1').style.opacity = 1
+  } else {
+    document.getElementById('pop1').style.opacity = 0
+    document.getElementById('pop2').style.opacity = 1
+  }
+}
+const firstPlayer = () => {
+  if (Math.round(Math.random()) = 0) {
+    isPlayerTurn = true;
+  } else {
+    isPlayerTurn = false;
+  }
+};
 
+//restart function
+const restart = () => {
+  for (let i = 0; i < 9; i++) {
+    document.querySelector(`#sq${i}`).setAttribute('data-increment', 0)
+    document.querySelector(`#sq${i}`).innerText = ''
+    let obj = {
+      spot: `${i}`,
+      mark: ''
+    };
+    boardTracker.splice(`${i}`, 1, obj)
+    document.getElementById(`sq${i}`).style.backgroundColor = ''
+  }
+  firstPlayer()
+  turnSign()
+}
+
+//reset function
+const reset = () => {
+  for (let i = 0; i < 9; i++) {
+    document.querySelector(`#sq${i}`).setAttribute('data-increment', 0)
+    document.querySelector(`#sq${i}`).innerText = ''
+    let obj = {
+      spot: `${i}`,
+      mark: ''
+    };
+    boardTracker.splice(`${i}`, 1, obj)
+    playerScore = 0;
+    cpuScore = 0;
+    tieScore = 0;
+    document.getElementById('pscore').innerText = playerScore
+    document.getElementById('cscore').innerText = cpuScore
+    document.getElementById('tscore').innerText = tieScore
+    document.getElementById(`sq${i}`).style.backgroundColor = ''
+  }
+  firstPlayer();
+  turnSign();
+}
 
 ////////////////////////////////
 // Event Listeners Here
@@ -62,10 +131,11 @@ sq.forEach ( s =>
       document.querySelector(`#${location}`).setAttribute('data-increment', 1)
       isPlayerTurn = !isPlayerTurn
       let obj = {
-        spot: parseInt(location.charAt(2))-1,
+        spot: parseInt(location.charAt(2)),
         mark: 'X'
       }
-      boardTracker.splice(parseInt(location.charAt(2))-1, 1, obj)
+      boardTracker.splice(parseInt(location.charAt(2)), 1, obj)
+      document.getElementById(location).style.backgroundColor = 'blue'
       console.log(boardTracker)
       winnerCheck();
       } else {
@@ -74,10 +144,11 @@ sq.forEach ( s =>
             document.querySelector(`#${location}`).setAttribute('data-increment', 1)
             isPlayerTurn = !isPlayerTurn
             let obj = {
-              spot: parseInt(location.charAt(2))-1,
+              spot: parseInt(location.charAt(2)),
               mark: 'O'
             }
-            boardTracker.splice(parseInt(location.charAt(2))-1, 1, obj)
+            boardTracker.splice(parseInt(location.charAt(2)), 1, obj)
+            document.getElementById(location).style.backgroundColor = 'yellow'
             console.log(boardTracker)
             winnerCheck();
             }
@@ -86,25 +157,9 @@ sq.forEach ( s =>
   )
 )
 
-
+let prompt = confirm(`Would you like to play a game of Tic Tac Toe?`)
+if (prompt) {
+  reset()
+}
+btn.addEventListener('click', reset)
 ////////////////////////////////
-
-// const balls = document.querySelectorAll('.js-ball');
-// const scoreDisplay = document.querySelector('.score')
-// const levelWinner = document.querySelector('.level-winner');
-// let score = 0
-
-// let ballscore = balls[0].getAttribute('data-increment')
-
-
-// balls.forEach( b => 
-
-//   b.addEventListener('click', () => {
-//     let ballscore = b.getAttribute('data-increment')
-//     score += parseInt(ballscore);
-//     scoreDisplay.innerText = score
-//   if (score >=100) {
-//     levelWinner.style.opacity = 1
-//   }
-//   })
-// )
